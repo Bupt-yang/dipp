a9a1e376cef5cdfb86ad1f081ddafb8497d1771c
Bupt-yang <55079544+Bupt-yang@users.noreply.github.com>
2020-06-27 18:10:41 +0800

    Add files via upload

---
+++ server/db.js
@@ -0,0 +1,10 @@
+// 数据库连接配置
+module.exports = {
+    mysql: {
+        host: 'localhost',
+        user: 'root',
+        password: 'root123',
+        database: 'test',
+        port: '3306'
+    }
+}
